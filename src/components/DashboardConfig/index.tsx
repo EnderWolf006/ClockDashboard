def0451329eef77b3a7eecd5654fb2c3df325e85
Ender Wolf <enderwolf006@gmail.com>
2024-06-24 20:04:49 +0800

    first commit

---
+++ src/components/DashboardConfig/index.tsx
@@ -0,0 +1,135 @@
+import React, { useCallback, useEffect, useImperativeHandle, useMemo, useState } from "react";
+import "./style.css";
+import { DashboardState, FieldType, base, bitable, dashboard } from '@lark-base-open/js-sdk';
+import { Button, Checkbox, Input, Select, Toast } from "@douyinfe/semi-ui";
+import timezoneZH from '../../timezone/timezone_zh.json';
+import timezoneEN from '../../timezone/timezone_en.json';
+import timezoneJA from '../../timezone/timezone_ja.json';
+import dialConfig from "../DashboardView/dialConfig.json";
+
+
+
+let isDark = false
+bitable.bridge.getTheme().then((theme: string) => {
+  isDark = theme.toLocaleLowerCase() == 'dark'
+})
+
+function IconSelect({ optionList, onChange, value }: any) {
+  return <>
+    <div className="iconSelectContainer">
+      {
+        optionList.map((item: any) => {
+          const prefix = isDark ? '/dark_' : '/'
+          if (item.value === value)
+            return (
+              <div className="iconSelectItem selected" onClick={(e) => onChange(item.value)}>
+                <img className="iconSelectIcon" src={prefix + item.selectedIcon}></img>
+                <div className="iconSelectLabel">{item.label}</div>
+              </div>
+            )
+          return (
+            <div className="iconSelectItem" onClick={(e) => onChange(item.value)}>
+              <img className="iconSelectIcon" src={prefix + item.icon}></img>
+              <div className="iconSelectLabel">{item.label}</div>
+            </div>
+          )
+        })
+      }
+    </div>
+  </>
+}
+
+function DashboardConfig(props: any, ref: any) {
+  const isCreate = dashboard.state === DashboardState.Create
+  const { config, setConfig, t, onConfigChange } = props;
+
+  let [timezoneOptionList, setTimezoneOptionList] = useState(timezoneEN) as any
+
+  bitable.bridge.getLanguage().then((lng) => {
+    setTimezoneOptionList(lng === 'zh' ? timezoneZH : lng === 'ja' ? timezoneJA : timezoneEN)
+  });
+
+  const clockTypeOptionList = [
+    {
+      label: t('clockType.traditional'),
+      value: 'traditional',
+      icon: 'classic_default.svg',
+      selectedIcon: 'classic_selected.svg',
+    },
+    {
+      label: t('clockType.digital'),
+      value: 'digital',
+      icon: 'electronic_default.svg',
+      selectedIcon: 'electronic_selected.svg',
+    }
+  ]
+
+  const digitalClockModeOptionList = [
+    {
+      label: t('digitalClock.mode.12'),
+      value: 12
+    },
+    {
+      label: t('digitalClock.mode.24'),
+      value: 24
+    }
+  ]
+
+  const digitalClockFontOptionList = [
+    {
+      label: 'JetBrainsMono',
+      value: 'jetB'
+    },
+    {
+      label: 'FusionPixel',
+      value: 'fPixel'
+    }
+  ]
+
+  const traditionalClockSkinOptionList = Object.keys(dialConfig).map((item: any) => {
+    return {
+      label: item,
+      value: item
+    }
+  })
+
+  useImperativeHandle(ref, () => ({
+    handleSetConfig() {
+      // 当确认按钮点击时被Dashboard调用
+      return config
+    }
+  }));
+
+  return (
+    <>
+      <div className="prompt">{t('timezone')}</div>
+      <Select placeholder={t('placeholder.pleaseSelect')} className="select" optionList={timezoneOptionList} onChange={(e) => { setConfig({ ...config, common: { ...config.common, timezone: e } }) }} value={config.common.timezone}></Select>
+
+      <div className="prompt">{t('clockType')}</div>
+      <IconSelect optionList={clockTypeOptionList} onChange={(e: any) => { setConfig({ ...config, common: { ...config.common, clockType: e } }) }} value={config.common.clockType} />
+
+      <Checkbox className="select" onChange={(e) => { setConfig({ ...config, common: { ...config.common, precision: e.target.checked } }) }} checked={config.common.precision}>{config.common.clockType !== 'traditional' ? t('precision') : t('precision_d')}</Checkbox >
+
+      {
+        config.common.clockType === 'traditional'
+          ?
+          <>
+            {config.common.precision && <Checkbox className="select" onChange={(e) => { setConfig({ ...config, traditionalClock: { ...config.traditionalClock, secAnimationSmooth: e.target.checked } }) }} checked={config.traditionalClock.secAnimationSmooth}>{t('traditionalClock.secHandSmooth')}</Checkbox >}
+
+            <div className="prompt">{t('traditionalClock.skin')}</div>
+            <Select placeholder={t('placeholder.pleaseSelect')} className="select" optionList={traditionalClockSkinOptionList} onChange={(e) => { setConfig({ ...config, traditionalClock: { ...config.traditionalClock, skin: e } }) }} value={config.traditionalClock.skin}></Select>
+          </>
+          :
+          <>
+            <div className="prompt">{t('digitalClock.mode')}</div>
+            <Select placeholder={t('placeholder.pleaseSelect')} className="select" optionList={digitalClockModeOptionList} onChange={(e) => { setConfig({ ...config, digitalClock: { ...config.digitalClock, mode: e } }) }} value={config.digitalClock.mode}></Select>
+
+            <div className="prompt">{t('digitalClock.font')}</div>
+            <Select placeholder={t('placeholder.pleaseSelect')} className="select" optionList={digitalClockFontOptionList} onChange={(e) => { setConfig({ ...config, digitalClock: { ...config.digitalClock, font: e } }) }} value={config.digitalClock.font}></Select>
+          </>
+      }
+    </>
+  )
+}
+
+export default React.forwardRef(DashboardConfig)
---
+++ src/components/DashboardView/index.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import { bitable, dashboard } from '@lark-base-open/js-sdk';
+import { Tooltip } from "@douyinfe/semi-ui";
+import dialConfig from "./dialConfig.json";
+
+let timer = undefined as any;
+
+const dailResNameList = ['dial_1', 'dial_2', 'dial_3']
+
+const dailRes = (() => {
+  const res = {} as any;
+  for (const name of dailResNameList) {
+    res[name] = {
+      light: {},
+      dark: {}
+    }
+    res[name].light.dial = `/dialResourse/${name}/dial_light.svg`
+    res[name].light.hour = `/dialResourse/${name}/hour_light.svg`
+    res[name].light.min = `/dialResourse/${name}/min_light.svg`
+    res[name].light.sec = `/dialResourse/${name}/sec_light.svg`
+    res[name].dark.dial = `/dialResourse/${name}/dial_dark.svg`
+    res[name].dark.hour = `/dialResourse/${name}/hour_dark.svg`
+    res[name].dark.min = `/dialResourse/${name}/min_dark.svg`
+    res[name].dark.sec = `/dialResourse/${name}/sec_dark.svg`
+    res[name].config = dialConfig[name as 'dial_1']
+  }
+  return res;
+})();
+
+export function DashboardView(props: any) {
+  const { config, isConfig, t } = props;
+
+  const [theme, setTheme] = useState('light')
+  bitable.bridge.getTheme().then((theme: string) => {
+    setTheme(theme.toLocaleLowerCase())
+  })
+
+  const [time, setTime] = useState({
+    hour: 0,
+    min: 0,
+    dsec: 0,
+    sec: 0
+  }) as any;
+
+  useEffect(() => {
+    if (timer) clearInterval(timer)
+    timer = setInterval(() => {
+      const _time = (() => {
+        const now = new Date();
+        return new Date(now.getTime() + (config.common.timezone - now.getTimezoneOffset() / -60) * 60 * 60 * 1000);
+      })();
+      const hour = _time.getHours();
+      const min = _time.getMinutes();
+      const sec = _time.getSeconds();
+      if (sec == 0) {
+        // 在平滑旋转时，秒针如果直接从60 - 0会出现逆时针动画，dsec 为 经典表盘专属属性
+        setTime({ hour, min, dsec: -1, sec });
+        requestAnimationFrame(() => {
+          setTime({ hour, min, dsec: 0, sec });
+        })
+      } else
+        setTime({ hour, min, dsec: sec, sec });
+
+    }, 1000)
+  }, [config.common.timezone])
+
+  return (
+    <>
+      {
+        config.common.clockType == 'traditional' ?
+          <div className="space" style={{ '--bgc': dailRes[config.traditionalClock.skin].config.backgroudColor } as any}>
+            <div className="t-clock">
+              <img className="dial" src={dailRes[config.traditionalClock.skin][theme].dial} />
+              {config.common.precision && <img className="hand" src={dailRes[config.traditionalClock.skin][theme].sec} style={{
+                'zIndex': 4,
+                '--rot': `${time.dsec / 60 * 360}deg`,
+                "--originOffset": dailRes[config.traditionalClock.skin].config.hand.sec.originOffset,
+                "--length": dailRes[config.traditionalClock.skin].config.hand.sec.length,
+                "--width": dailRes[config.traditionalClock.skin].config.hand.sec.width,
+                "transition": config.traditionalClock.secAnimationSmooth && time.dsec != -1 ? 'transform 1s linear' : 'none'
+              } as any} />}
+              <img className="hand" src={dailRes[config.traditionalClock.skin][theme].min} style={{
+                'zIndex': 3,
+                '--rot': `${(time.min + time.sec / 60) / 60 * 360}deg`,
+                "--originOffset": dailRes[config.traditionalClock.skin].config.hand.min.originOffset,
+                "--length": dailRes[config.traditionalClock.skin].config.hand.min.length,
+                "--width": dailRes[config.traditionalClock.skin].config.hand.min.width
+              } as any} />
+              <img className="hand" src={dailRes[config.traditionalClock.skin][theme].hour} style={{
+                'zIndex': 2,
+                '--rot': `${((time.hour >= 12 ? time.hour - 12 : time.hour) + time.min / 60) / 12 * 360}deg`,
+                "--originOffset": dailRes[config.traditionalClock.skin].config.hand.hour.originOffset,
+                "--length": dailRes[config.traditionalClock.skin].config.hand.hour.length,
+                "--width": dailRes[config.traditionalClock.skin].config.hand.hour.width
+              } as any} />
+            </div>
+          </div> :
+          <div className="space">
+            <div className="d-clock" style={{ fontFamily: config.digitalClock.font, fontSize: isConfig ? '10vh' : '90vh' }}>
+              <div className="number">{(config.digitalClock.mode == 24 ? time.hour : time.hour >= 12 ? time.hour - 12 : time.hour).toString().padStart(2, 0)}</div>
+              <div className="number">:</div>
+              <div className="number">{time.min.toString().padStart(2, 0)}</div>
+              {config.common.precision && <>
+                <div className="number">:</div>
+                <div className="number">{time.sec.toString().padStart(2, 0)}</div>
+              </>
+              }
+              {
+                config.digitalClock.mode == 12 && <div className="suffix" style={{ fontSize: isConfig ? '3vh' : '20vh' }}>{time.hour >= 12 ? 'PM' : 'AM'}</div>
+              }
+            </div>
+          </div>
+      }
+
+    </>
+  )
+}
